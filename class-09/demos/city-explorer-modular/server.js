1272ec8ea1b9ad1bece713d9c35bf55599beeed5
JB Tellez <jb.tellez@gmail.com>
2019-06-14 09:24:32 -0700

    Demo 09 (#29)
    * adds in class city expolorer modular changes
    
    * adds notes and tweaks

--- class-09/demos/city-explorer-modular/server.js
+++ class-09/demos/city-explorer-modular/server.js
@@ -35,35 +35,40 @@ const getMovies = require('./modules/movies');
 // route handlers
 function handleLocation(req, res) {
 
-    getLocation(req.query.data, client, superagent)
-        .then(location => res.send(location))
-        .catch(error => handleError(error, res));
+  getLocation(req.query.data, client, superagent)
+    .then(location => res.send(location))
+    .catch(error => handleError(error, res));
 }
 
 function handleWeather(req, res) {
 
-    getForecasts(req.query.data.latitude, req.query.data.longitude, client, superagent)
-        .then(forecasts => res.send(forecasts))
-        .catch(error => handleError(error, res));
+  // are we getting the location Id here, we're gonna need it
+  // so check the queryString and/or view the city-explorer front end code
+  // and make it work! ;)
+  console.log('************* handle weather', req.query.data);
+
+  getForecasts(req.query.data.latitude, req.query.data.longitude, client, superagent)
+    .then(forecasts => res.send(forecasts))
+    .catch(error => handleError(error, res));
 }
 
 function handleEvents(req, res) {
 
-    getEvents(req.query.data.formatted_query, client, superagent)
-        .then(events => res.send(events))
-        .catch(error => handleError(error, res));
+  getEvents(req.query.data.formatted_query, client, superagent)
+    .then(events => res.send(events))
+    .catch(error => handleError(error, res));
 }
 
 function handleMovies(req, res) {
 
-    getMovies(req.query.data.search_query, req.query.data.id, client, superagent)
-        .then(movies => res.send(movies))
-        .catch(error => handleError(error, res));
+  getMovies(req.query.data.search_query, req.query.data.id, client, superagent)
+    .then(movies => res.send(movies))
+    .catch(error => handleError(error, res));
 }
 
 function handleError(error, response) {
-    console.error(error);
-    response.status(500).send('ERROR');
+  console.error(error);
+  response.status(500).send('ERROR');
 }
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
